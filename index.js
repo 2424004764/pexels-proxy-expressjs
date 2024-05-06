0a19edfee01e7309abe471e48fdf131e393b070e
yifang <2424004764@qq.com>
2024-05-06 12:40:18 +0800

    proxy test

--- index.js
+++ index.js
@@ -1,58 +1,30 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
-
-const logger = morgan("tiny");
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+// 创建一个代理中间件，将 folgode.com 的请求代理到 aa.com
+const proxyMiddleware = createProxyMiddleware({
+  target: 'images.pexels.com',  // 目标代理地址
+  changeOrigin: true,        // 更改请求头中的 Origin
+  // 其他可选配置项
 });
 
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
+// 使用代理中间件来处理所有 folgode.com 的请求
+app.use('*', (req, res, next) => {
+  // 检查请求的主机名是否为 folgode.com
+  if (req.hostname === '	pexels-proxy.folgode.com') {
+    // 使用代理中间件处理请求
+    proxyMiddleware(req, res, next);
+  } else {
+    // 如果请求的主机名不是 folgode.com，则继续正常的请求处理
+    next();
   }
 });
-
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
+  // await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
