9f6cc628defad6952eb1feec5bd175bc99e3fcf9
yifang <2424004764@qq.com>
2024-05-06 13:21:58 +0800

    debug

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const app = express();
 
 // 创建一个代理中间件，将以 /photos 开头的请求代理到目标地址
 const photosProxy = createProxyMiddleware('/photos', {
-  target: 'http://aa.com',  // 目标代理地址
+  target: 'images.pexels.com/photos',  // 目标代理地址
   changeOrigin: true,        // 更改请求头中的 Origin
-  pathRewrite: {
-    // 将 /photos 开头的请求重写为目标地址的不同路径（可选）
-    '^/photos': '/photos',  // 如果目标地址不需要重写路径，可以为空字符串
-  },
 });
 
 // 使用代理中间件来处理以 /photos 开头的请求
