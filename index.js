1da2adcb2f82ddab229868ed8673271ddc316825
yifang <2424004764@qq.com>
2024-05-07 11:35:04 +0800

    22

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ try {
   // 应用启动逻辑
 
   // 创建一个代理中间件，将以 /photos 开头的请求代理到目标地址
-  const photosProxy = createProxyMiddleware('/photos', {
-    target: "http://images.pexels.com"
-  });
+  // const photosProxy = createProxyMiddleware('/photos', {
+  //   target: "http://images.pexels.com"
+  // });
 
-  // 使用代理中间件来处理以 /photos 开头的请求
-  app.use('/photos', photosProxy);
+  // // 使用代理中间件来处理以 /photos 开头的请求
+  // app.use('/photos', photosProxy);
 
   // 根目录路由处理函数，输出当前时间
   app.get('/', (req, res) => {
