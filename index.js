59e614d7c346a52b2f8236e6929869e29a74f3b3
yifang <2424004764@qq.com>
2024-06-06 17:28:10 +0800

    新增videos转发

--- index.js
+++ index.js
@@ -19,8 +19,25 @@ try {
     },
   });
 
+    // 创建一个代理中间件，将以 /videos 开头的请求代理到目标地址
+    const videosProxy = createProxyMiddleware({
+      target: "http://images.pexels.com/videos",
+      changeOrigin: true, // 设置更改请求头中的 Origin
+      pathRewrite: {
+        // 将 /photos 开头的请求重写为目标地址的不同路径，保留 /videos
+        '^/videos': '' // 保留 /videos 部分，只替换后面的部分
+      },
+      onProxyRes: function (proxyRes, req, res) {
+        // 修改响应头信息，隐藏跳转细节
+        proxyRes.headers['X-Forwarded-Host'] = req.headers['host'];
+        proxyRes.headers['X-Forwarded-Proto'] = req.headers['x-forwarded-proto'] || 'http';
+      },
+    });
+
   // 使用代理中间件来处理以 /photos 开头的请求
   app.use('/photos', photosProxy);
+  // 使用代理中间件来处理以 /photos 开头的请求
+  app.use('/videos', videosProxy);
 
   const photosApiProxy = createProxyMiddleware({
     target: "https://api.pexels.com",
